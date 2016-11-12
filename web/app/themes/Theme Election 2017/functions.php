fda21556d5772dd1b94f57d3f78f886ebe7afabb
T-Zahil <thomas.sanlis@gmail.com>
2016-11-12 10:18:58 +0100

    [global] Add custom post type : soutien

---
+++ web/app/themes/Theme Election 2017/functions.php
@@ -0,0 +1,25 @@
+<?php
+// Custom post types init
+include_once('inc/custom_post_types.php');
+// taxonomies init
+include_once('inc/taxonomies.php');
+// roles init
+include_once('inc/roles.php');
+// styles script init
+include_once('inc/scripts_styles.php');
+// theme support
+include_once('inc/theme_support.php');
+// ajax functions
+include_once('inc/ajax.php');
+
+
+add_shortcode('commentaire', "function_commentaire_shortcode" ); 
+
+function function_commentaire_shortcode($attr){
+  $attr = shortcode_atts( array(
+        'content' => 'Contenu par défaut',
+        'id' => '123' // id par défaut
+    ), $attr);
+  
+  return '<!-- commentaire '.$attr['id'].' : '.$attr['content'].'-->';
+}
---
+++ web/app/themes/Theme Election 2017/inc/custom_post_types.php
@@ -0,0 +1,52 @@
+<?php
+add_action('init', 'create_custom_post_type_soutiens');
+
+function create_custom_post_type_annonces(){
+  
+  $labels = array(
+        'name'               => 'Soutiens',
+        'singular_name'      => 'Soutien',
+        'all_items'          => 'Tous les soutiens',
+        'add_new'            => 'Ajouter un soutien',
+        'add_new_item'       => 'Ajouter un nouveau soutien',
+        'edit_item'          => "Modifier le soutien",
+        'new_item'           => 'Nouveau soutien',
+        'view_item'          => "Voir le soutien",
+        'search_items'       => 'Rechercher un soutien',
+        'not_found'          => 'Pas de résultat',
+        'not_found_in_trash' => 'Pas de résultat',
+        'parent_item_colon'  => 'soutien parent:',
+        'menu_name'          => 'Soutiens',
+    );
+
+    $args = array(
+        'labels'              => $labels,
+        'hierarchical'        => false,
+        'supports'            => array( 'title','thumbnail','editor', 'excerpt', 'comments'),
+        'public'              => true,
+        'show_ui'             => true,
+        'show_in_menu'        => true,
+        'menu_position'       => -1,
+        'menu_icon'           => 'dashicons-megaphone',
+        'show_in_nav_menus'   => true,
+        'publicly_queryable'  => true,
+        'exclude_from_search' => false,
+        'has_archive'         => false,
+        'query_var'           => true,
+        'can_export'          => true,
+        'rewrite'             => array( 'slug' => 'annonce' ),
+        'capabilities' => array(
+                          'publish_posts' => 'publish_soutien',
+                          'edit_posts' => 'edit_soutien',
+                          'edit_others_posts' => 'edit_others_soutiens',
+                          'delete_posts' => 'delete_soutien',
+                          'delete_others_posts' => 'delete_others_soutien',
+                          'read_private_posts' => 'read_private_soutien',
+                          'edit_post' => 'edit_soutien',
+                          'delete_post' => 'delete_soutien',
+                          'read_post' => 'read_soutien'
+                              ),
+
+    );
+    register_post_type( 'soutien', $args );
+}
