3af2a4a31ce3b0ca0975969fe4d966cf6ba7aa45
T-Zahil <thomas.sanlis@gmail.com>
2016-11-14 15:00:51 +0100

    [home] Merge index.html into index.php

--- web/app/themes/Theme Election 2017/footer.php
+++ web/app/themes/Theme Election 2017/footer.php
@@ -1,5 +1,14 @@
+    <script src="<?php bloginfo('template_url'); ?>/script.js"></script>
+    <script>
+      document.addEventListener('DOMContentLoaded', function() {
+        var simple = document.querySelector('.js_simple');
+        lory(simple, {
+          infinite: 1
+      });
+    });
+</script>
 <footer id="footer">
 </footer>
-    <?php wp_footer();  ?>
-    </body>
+<?php wp_footer();  ?>
+</body>
 </html>
--- web/app/themes/Theme Election 2017/header.php
+++ web/app/themes/Theme Election 2017/header.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style.css">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css" media="screen" title="no title">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700,900" rel="stylesheet">
     <title>Election Présidentielle 2017 - Le Candidat Blanc</title>
     <?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?>>
-    <header id="header">
-    </header>
+
+<body class="homepage">
+    <nav>
+
+    </nav>
