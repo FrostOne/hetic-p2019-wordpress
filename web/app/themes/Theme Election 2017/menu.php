42f554fb2071fe779b9f6942e2e52db062de8110
simschreib <simon.schreiber@laposte.net>
2016-11-16 00:39:14 +0100

    [front]
    4 pages
    + nav
    + footer

---
+++ web/app/themes/Theme Election 2017/menu.php
@@ -0,0 +1,20 @@
+ <nav class="row">
+    <img src="images/logo.png">
+    <div class="col-1"></div>
+    <div class="col-7 ma">
+      <ul>
+        <li>ACCUEIL</li>
+        <li>POURQUOI VOTER BLANC</li>
+        <li>LE PROGRAMME</li>
+        <li>COMITÉ DE SOUTIEN</li>
+        <li>BLOG</li>
+        <li>CONTACT</li>
+      </ul>
+
+    </div>
+    <div class="col-3 timer ma">
+      COMPTE À REBOURD ÉLÉCTORALE
+      <div id="coutdown"></div>
+    </div>
+    
+  </nav>
