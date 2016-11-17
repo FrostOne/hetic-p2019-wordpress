e5240d8ac4f470f71f928ed1c27d5629e52d356c
T-Zahil <thomas.sanlis@gmail.com>
2016-11-17 12:21:14 +0100

    [menu] Add menu links

--- web/app/themes/Theme Election 2017/header.php
+++ web/app/themes/Theme Election 2017/header.php
@@ -2,32 +2,32 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css" media="screen" title="no title">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700,900" rel="stylesheet">
-    <title>Election Présidentielle 2017 - Le Candidat Blanc</title>
-    <?php wp_head(); ?>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css" media="screen" title="no title">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700,900" rel="stylesheet">
+  <title>Election Présidentielle 2017 - Le Candidat Blanc</title>
+  <?php wp_head(); ?>
 </head>
 
 <body class="homepage">
- <nav class="row">
-    <img src="<?php bloginfo('template_directory'); ?>/images/logo.png">
-    <div class="col-1"></div>
-    <div class="col-7 ma">
-      <ul>
-        <li>ACCUEIL</li>
-        <li>POURQUOI VOTER BLANC</li>
-        <li>LE PROGRAMME</li>
-        <li>COMITÉ DE SOUTIEN</li>
-        <li>BLOG</li>
-        <li>CONTACT</li>
+   <nav class="row">
+      <img src="<?php bloginfo('template_directory'); ?>/images/logo.png">
+      <div class="col-1"></div>
+      <div class="col-7 ma">
+        <ul>
+          <li><a href="<?php echo home_url(); ?>" title="Accueil">ACCUEIL</a></li>
+          <li><a href="?page_id=56" title="Pourquoi voter blanc">POURQUOI VOTER BLANC</a></li>
+          <li><a href="?page_id=52" title="Le Programme">LE PROGRAMME</a></li>
+          <li><a href="?page_id=54" title="Comité de soutien">COMITÉ DE SOUTIEN</a></li>
+          <li><a href="?page_id=60" title="Blog">BLOG</a></li>
+          <li><a href="?page_id=50" title="Contact">CONTACT</a></li>
       </ul>
 
-    </div>
-    <div class="col-3 timer ma">
-      COMPTE À REBOURD ÉLÉCTORALE
-      <div id="coutdown"></div>
-    </div>
-  </nav>
+  </div>
+  <div class="col-3 timer ma">
+    COMPTE À REBOURS ÉLÉCTORAL
+    <div id="coutdown"></div>
+</div>
+</nav>
--- web/app/themes/Theme Election 2017/menu.php
+++ web/app/themes/Theme Election 2017/menu.php
@@ -3,17 +3,17 @@
     <div class="col-1"></div>
     <div class="col-7 ma">
       <ul>
-        <li>ACCUEIL</li>
-        <li>POURQUOI VOTER BLANC</li>
-        <li>LE PROGRAMME</li>
-        <li>COMITÉ DE SOUTIEN</li>
-        <li>BLOG</li>
-        <li>CONTACT</li>
+        <li><a href="" title="Accueil">ACCUEIL</a></li>
+        <li><a href="" title="Pourquoi voter blanc">POURQUOI VOTER BLANC</a></li>
+        <li><a href="" title="Le Programme"></a>LE PROGRAMME</li>
+        <li><a href="" title="Comité de soutien"></a>COMITÉ DE SOUTIEN</li>
+        <li><a href="/?page_id=60" title="Blog">BLOG</a></li>
+        <li><a href="" title="Contact">CONTACT</a></li>
       </ul>
 
     </div>
     <div class="col-3 timer ma">
-      COMPTE À REBOURD ÉLÉCTORALE
+      COMPTE À REBOURS ÉLÉCTORAL
       <div id="coutdown"></div>
     </div>
   </nav>
