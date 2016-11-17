e5c32b774f095eada044972c032d577cf0f510e4
T-Zahil <thomas.sanlis@gmail.com>
2016-11-17 12:22:10 +0100

    [footer] Add footer links

--- web/app/themes/Theme Election 2017/footer.php
+++ web/app/themes/Theme Election 2017/footer.php
@@ -11,16 +11,16 @@
     <div class="row">
         <div class="col-8">
           <ul>
-            <li>ACCUEIL</li>
-            <li>POURQUOI VOTER BLANC</li>
-            <li>LE PROGRAMME</li>
-            <li>COMITÉ DE SOUTIEN</li>
-            <li>BLOG</li>
-            <li>CONTACT</li>
-        </ul>
-    </div>
-    <div class="col-4 left">© Copyright 2016 - HETIC</div>
-</div>
+              <li><a href="<?php echo home_url(); ?>" title="Accueil">ACCUEIL</a></li>
+              <li><a href="?page_id=56" title="Pourquoi voter blanc">POURQUOI VOTER BLANC</a></li>
+              <li><a href="?page_id=52" title="Le Programme">LE PROGRAMME</a></li>
+              <li><a href="?page_id=54" title="Comité de soutien">COMITÉ DE SOUTIEN</a></li>
+              <li><a href="?page_id=60" title="Blog">BLOG</a></li>
+              <li><a href="?page_id=50" title="Contact">CONTACT</a></li>
+          </ul>
+      </div>
+      <div class="col-4 left">© Copyright 2016 - HETIC</div>
+  </div>
 </footer>
 </body>
 </html>
